4d6f558a79e64cc49601fe17e04d6757a3e53c25
Kirill Petrosian <kiril.petrosian@wsc-sports.com>
2022-05-07 12:06:30 +0400

    regions for statistics

--- src/components/pages/clothes-choice/statistics/maps/WaypointMap.js
+++ src/components/pages/clothes-choice/statistics/maps/WaypointMap.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useRef, useState } from "react";
 import { connect } from "react-redux";
+import { ALERT_TYPES, PERM_LOCATION } from "../../../../../constants";
 import mapDispatchToProps from "../../../../../store/actions";
 
-const WaypointMap = ({ waypoint, statisticsActions }) => {
+const WaypointMap = ({ waypoint, statisticsActions, alertsActions }) => {
   const mapRef = useRef(null);
   const multiRouteRef = useRef(null);
+  const regionsRef = useRef(null);
+
+  const previousValidReferencePointsRef = useRef([]);
 
   useEffect(() => {
     window.ymaps.ready(() => {
@@ -14,33 +18,41 @@ const WaypointMap = ({ waypoint, statisticsActions }) => {
             coords: { latitude, longitude },
           } = position;
 
-          mapRef.current = new window.ymaps.Map("waypointMap", {
-            center: waypoint?.coordinates || [latitude, longitude],
-            zoom: 9,
-            controls: [],
+          window.ymaps.borders.load("RU").then((geoJson) => {
+            mapRef.current = new window.ymaps.Map("waypointMap", {
+              center: waypoint?.coordinates || PERM_LOCATION,
+              zoom: 9,
+              controls: [],
+            });
+
+            const regions = window.ymaps
+              .geoQuery(geoJson)
+              .setOptions({ fillColor: "#dceef4", fillOpacity: "0.6" });
+            regions.addToMap(mapRef.current);
+            regionsRef.current = regions;
+
+            var multiRoute = new window.ymaps.multiRouter.MultiRoute(
+              {
+                referencePoints: [waypoint?.coordinates || PERM_LOCATION],
+              },
+              {
+                editorDrawOver: false,
+                editorMidPointsType: "way",
+              }
+            );
+
+            multiRouteRef.current = multiRoute;
+
+            multiRoute.model.events.add("requestsuccess", onWayPointsChange);
+
+            multiRoute.editor.start({
+              addWayPoints: false,
+              removeWayPoints: false,
+              addMidPoints: false,
+            });
+
+            mapRef.current.geoObjects.add(multiRoute);
           });
-
-          var multiRoute = new window.ymaps.multiRouter.MultiRoute(
-            {
-              referencePoints: [waypoint?.coordinates || [latitude, longitude]],
-            },
-            {
-              editorDrawOver: false,
-              editorMidPointsType: "way",
-            }
-          );
-
-          multiRouteRef.current = multiRoute;
-
-          multiRoute.model.events.add("requestsuccess", onWayPointsChange);
-
-          multiRoute.editor.start({
-            addWayPoints: false,
-            removeWayPoints: false,
-            addMidPoints: false,
-          });
-
-          mapRef.current.geoObjects.add(multiRoute);
         },
         () => alert("You need to enable geolocation")
       );
@@ -50,9 +62,20 @@ const WaypointMap = ({ waypoint, statisticsActions }) => {
   const onWayPointsChange = () => {
     const newWaypoints = multiRouteRef.current.getWayPoints();
     const newWaypointsMapped = [];
+    let isInRegion = true;
 
     newWaypoints.each((point) => {
       const pointData = point.properties.getAll();
+
+      if (regionsRef.current) {
+        isInRegion =
+          isInRegion &&
+          Boolean(regionsRef.current.searchContaining(point).get(0));
+
+        if (!isInRegion) {
+          return;
+        }
+      }
 
       newWaypointsMapped.push({
         index: pointData.index,
@@ -60,7 +83,22 @@ const WaypointMap = ({ waypoint, statisticsActions }) => {
       });
     });
 
-    statisticsActions.setWaypoint(newWaypointsMapped[0]);
+    if (isInRegion) {
+      statisticsActions.setWaypoint(newWaypointsMapped[0]);
+      previousValidReferencePointsRef.current = newWaypointsMapped;
+    } else {
+      alertsActions.showAlert(
+        "You are not allowed to set points outside your region!",
+        ALERT_TYPES.ERROR
+      );
+      if (previousValidReferencePointsRef.current.length) {
+        multiRouteRef.current.model.setReferencePoints(
+          previousValidReferencePointsRef.current.map(
+            ({ coordinates }) => coordinates
+          )
+        );
+      }
+    }
   };
 
   return <div id="waypointMap" className="w-100 h-100"></div>;
